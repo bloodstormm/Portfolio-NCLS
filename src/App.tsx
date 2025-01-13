7b4f3f5dced94ca2207284d84d8350ebc2640c64
bloodstormm <nicolasmalaquias2015@hotmail.com>
2025-01-13 14:17:32 -0300

    SpeedInsights Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RoutesContainer } from "./components/RoutesContainer";
 import { BrowserRouter as Router } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <div className="mx-auto w-full text-brown">
       <Router>
         <RoutesContainer />
+        <SpeedInsights />
       </Router>
     </div>
   );
